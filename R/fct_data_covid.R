0d7f851642db27aea21abc89bf2275ef4cc6eedf
Toan Tran <trannttoan97@gmail.com>
2025-02-10 16:40:00 -0500

    - remove code duplication in mode_analyze_upload - show input data validation feedback in the side panel instead of using a modal dialogue - change preprocessing code to include all tracts instead of only the ones in the urbanicity and ADI datasets - precompute all the zip-code-level covariates and poststratification data instead of on-demand

--- R/fct_data_covid.R
+++ R/fct_data_covid.R
@@ -125,7 +125,7 @@ aggregate_covid <- function(
   patient$time <- time_indices
 
   # remove all but one test of a patient in the same week
-  # patient <- patient |> distinct(id, time, .keep_all = TRUE)
+  patient <- patient |> distinct(id, time, .keep_all = TRUE)
 
   # impute missing demographic data based on frequency
   patient <- patient |> mutate(across(c(sex, race, age), impute))
@@ -259,113 +259,41 @@ get_dates <- function(df) {
 }
 
 filter_state_zip <- function(
-    df_zip,
-    zip_tract,
+    patient,
+    covariates,
     zip_threshold = 5,
     state_threshold = 0.01
 ) {
-
-  n_records <- sum(df_zip$zip_count, na.rm = TRUE)
+  
+  zip_count <- patient |>
+    group_by(zip) |>
+    summarize(count = sum(total))
 
   # create a table containing state, zip and zip count
-  state_zip <- zip_tract |>
-    mutate(state_code = substr(geoid, 1, 2)) |>
-    select(zip, state, state_code) |>
+  state_zip <- covariates |>
+    mutate(state = substr(county, 1, 2)) |>
+    select(zip, state) |>
     distinct(zip, .keep_all = TRUE) |>
-    right_join(df_zip, by = "zip")
+    inner_join(zip_count, by = "zip")
 
   # filter based on proportion of state
+  N <- sum(state_zip$count)
   state_zip <- state_zip |>
     group_by(state) |>
-    filter(sum(zip_count) > state_threshold * n_records) |>
+    filter(sum(count) > state_threshold * N) |>
     ungroup()
 
   # filter based on number of zip
   state_zip <- state_zip |>
     group_by(zip) |>
-    filter(sum(zip_count) > zip_threshold) |>
+    filter(sum(count) > zip_threshold) |>
     ungroup()
 
-  state_zip_select <- state_zip |>
-    select(state_code, zip) |>
-    sapply(unique)
+  patient <- patient |> filter(zip %in% state_zip$zip)
 
-  return(state_zip_select)
+  return(patient)
 }
 
-# For each zip, find tracts and aggregate data over them
-combine_tracts <- function(
-    patient,
-    tract_data,
-    zip_tract
-) {
-
-  by_zip <- zip_tract |>
-    filter(zip %in% unique(patient$zip)) |>
-    select(geoid, zip) |>
-    rename("GEOID" = "geoid") |>
-    inner_join(
-      tract_data,
-      by = "GEOID"
-    ) |>
-    mutate(county = substr(GEOID, 1, 5)) |>
-    group_by(zip)
-
-  all_colnames <- names(tract_data)
-  pstrat_colnames <- all_colnames[grepl("male|female", all_colnames)]
-  pstrat_data <- by_zip |>
-    summarise(
-      county = first(county),
-      across(all_of(pstrat_colnames), sum)
-    )
-
-  covariates <- by_zip |>
-    summarize(
-      county = first(county),
-      urbanicity  = 1 - sum((pop_size / sum(pop_size)) * (urbanicity == "N")),
-      college     = sum(above_college) / (sum(below_college) + sum(above_college)),
-      employment  = sum(employed) / (sum(employed) + sum(unemployed) + sum(other)),
-      poverty     = sum(`0-0.99`) / (sum(`0-0.99`) + sum(`1-1.99`) + sum(`2+`)),
-      income      = sum((pop_size / sum(pop_size)) * household_income),
-      ADI         = sum((pop_size / sum(pop_size)) * adi)
-    )
-
-  # remove zip codes without tract in USPS crosswalk table
-  patient <- patient |> filter(zip %in% pstrat_data$zip)
-
-  return(list(patient, pstrat_data, covariates))
-}
-
-link_ACS <- function(
-    patient,
-    tract_data,
-    zip_tract
-) {
-
-  # filter out state and zip codes with small sample sizes
-  state_zip_select <- filter_state_zip(
-    patient |> group_by(zip) |> summarize(zip_count = sum(total)),
-    zip_tract
-  )
-  patient <- patient |> filter(zip %in% state_zip_select$zip)
-
-  # compute zip-level data
-  c(patient, pstrat_data, covariates) %<-% combine_tracts(
-    patient,
-    tract_data,
-    zip_tract
-  )
-
-  # save unstandardized covariates for plotting
-  raw_covariates <- covariates
-
-  # standardize zip-level covariates
-  covariates <- covariates |> mutate(
-    across(!zip & !county, function(c) c |> scale() |> as.vector() |> unlist(use.names=FALSE))
-  )
-
-  return(list(patient, pstrat_data, covariates, raw_covariates))
-}
 
 prepare_data_covid <- function(
     patient,
@@ -373,14 +301,23 @@ prepare_data_covid <- function(
     covariates,
     demo_levels
 ) {
-
+  
+  # filter out state and zip codes with small sample sizes
+  patient <- filter_state_zip(patient, covariates)
+  
+  pstrat_data <- pstrat_data |> filter(zip %in% patient$zip)
   cell_counts <- pstrat_data[-c(1, 2)] |> t() |> c()
   time_indices <- 1:max(patient$time)
   n_time_indices <- length(time_indices)
-
+  
+  
+  # standardize zip-level covariates
+  covariates <- covariates |>
+    filter(zip %in% patient$zip) |>
+    mutate(across(-c(zip, county), scale))
+  
   input_data <- patient |>
     left_join(covariates, by = "zip")
-
 
   new_data <- tidyr::expand_grid(
     sex  = demo_levels$sex,
@@ -470,25 +407,25 @@ get_tract_data <- function(
     poverty_bounds,
     year
 ) {
-
+  
   gen_vars <- function(str, seq) seq |> sapply(\(i) sprintf("%s_%03d", str, i))
-
+  
   # look-up table
   # https://www.census.gov/programs-surveys/acs/data/data-tables/table-ids-explained.html
   lookup_df <- load_variables(2021, dataset = "acs5", cache = TRUE)
-
+  
   # the indices are chosen based on the look-up table
   # for extracting table labels from look-up table
   age_indices_one <- 4:16
   age_indices_all <- 283:304
   poverty_indices <- 26397:26402
-
+  
   # for level-based aggregation of columns of get_acs output
   education_levels <- c("below_college", "above_college")
   education_indices <- list(1:19, 20:24)
   employment_levels <- c("employed", "unemployed", "other")
   employment_indices <- list(3, 4, 5:6)
-
+  
   # generate variable names
   group_names <- c("male_white", "female_white",
                    "male_black", "female_black",
@@ -496,26 +433,26 @@ get_tract_data <- function(
                    "education", "poverty",
                    "employment", "income",
                    "pop_size")
-
+  
   group_prefixes <- c("B01001A", "B01001A",
                       "B01001B", "B01001B",
                       "B01001", "B01001",
                       "B15003", "C17002",
                       "B23025", "B19013",
                       "B01001")
-
+  
   group_table_numbers <- list(3:16, 18:31,
                               3:16, 18:31,
                               3:25, 27:49,
                               2:25, 2:8,
                               2:7, 1,
                               1)
-
+  
   group_vars <- c()
   for(i in 1:length(group_names)) {
     group_vars <- group_vars |> c(gen_vars(group_prefixes[i], group_table_numbers[[i]]))
   }
-
+  
   # retrieve ACS tables
   all_tables <- get_acs(
     geography = "tract",
@@ -524,10 +461,10 @@ get_tract_data <- function(
     output = "wide",
     year = year
   )
-
+  
   geoID <- all_tables[1]
   all_tables <- all_tables[seq(3, ncol(all_tables), by = 2)]
-
+  
   group_dfs <- list()
   ind <- 0
   for(i in 1:length(group_names)) {
@@ -535,19 +472,19 @@ get_tract_data <- function(
     group_dfs[group_names[i]] <- all_tables[(ind+1):(ind+n_tables)] |> list()
     ind <- ind + n_tables
   }
-
-
+  
+  
   df_all <- data.frame(GEOID = geoID)
-
+  
   # POPULATION SIZE
   names(group_dfs$pop_size) <- "pop_size"
   df_all <- cbind(df_all, group_dfs$pop_size)
-
+  
   ### SEX, RACE, AGE
   # aggregate columns
   age_bounds_acs_all <- get_bounds(lookup_df$label[age_indices_all])
   age_bounds_acs_one <- get_bounds(lookup_df$label[age_indices_one])
-
+  
   for(name in c("male_all", "female_all")) {
     group_dfs[name] <- collapse(age_bounds,
                                 age_bounds_acs_all,
@@ -561,68 +498,67 @@ get_tract_data <- function(
                                 group_dfs[[name]],
                                 1) |> list()
   }
-
+  
   # subtract white and black from total
   group_dfs$male_other <- group_dfs$male_all - (group_dfs$male_white + group_dfs$male_black)
   group_dfs$female_other <- group_dfs$female_all - (group_dfs$female_white + group_dfs$female_black)
-
+  
   # rename columns and combine data frames
   for(name in c("male_white", "male_black", "male_other",
                 "female_white", "female_black", "female_other")) {
     names(group_dfs[[name]]) <- paste0(name, '_', names(group_dfs[[name]]))
     df_all <- cbind(df_all, group_dfs[[name]])
   }
-
-  # URBANICITY
-  urbanicity <- read_sas("z_us_tract_uac.sas7bdat") |>
-    rename(
-      "urbanicity" = "uac_yn",
-      "GEOID" = "geocode"
-    )
-  df_all <- df_all |> inner_join(urbanicity, by = "GEOID")
-
-
+  
   # EDUCATION
   for(i in 1:length(education_indices)) {
     name <- education_levels[i]
     inds <- education_indices[[i]]
     df_all[[name]] <- group_dfs$education[inds] |> rowSums()
   }
-
+  
   # POVERTY
   poverty_bounds_acs <- get_bounds(lookup_df$label[poverty_indices])
   group_dfs$poverty <- collapse(poverty_bounds,
                                 poverty_bounds_acs,
                                 group_dfs$poverty,
                                 0.01) |> list()
-
+  
   df_all <- cbind(df_all, group_dfs$poverty)
-
+  
   # EMPLOYMENT
   for(i in 1:length(employment_indices)) {
     name <- employment_levels[i]
     inds <- employment_indices[[i]]
     df_all[[name]] <- group_dfs$employment[inds] |> rowSums()
   }
-
+  
   # INCOME
   names(group_dfs$income) <- "household_income"
   df_all <- cbind(df_all, group_dfs$income)
-
+  
+  # URBANICITY
+  urbanicity <- haven::read_sas("z_us_tract_uac.sas7bdat") |>
+    rename(
+      "urbanicity" = "uac_yn",
+      "GEOID" = "geocode"
+    ) |>
+    select(GEOID, urbanicity)
+  df_all <- df_all |> full_join(urbanicity, by = "GEOID")
+  
   # ADI
-  adi_data <- read_sas("z_adi_bg_v3_2019.sas7bdat") |>
+  adi_data <- haven::read_sas("z_adi_bg_v3_2019.sas7bdat") |>
     na.omit() |>
     group_by(state_cty_tract_cd) |>
     summarize(
       adi = mean(us_adi_rank_num)
     ) |>
     rename("GEOID" = "state_cty_tract_cd")
-  df_all <- df_all |> inner_join(adi_data, by = "GEOID")
-
-  df_all <- na.omit(df_all)
-
+  df_all <- df_all |> full_join(adi_data, by = "GEOID")
+  
   return(df_all)
 }
+
 
 # Rettrieve USPS crosswalk table
 get_zip_tract <- function(key) {
@@ -635,4 +571,47 @@ get_zip_tract <- function(key) {
   zip_tract <- dplyr::bind_rows(output$data$results)
 
   return(zip_tract)
+}
+
+combine_tracts <- function(
+    tract_data,
+    zip_tract
+) {
+  
+  # join tract-level data with zip-tract conversion table
+  # then group by zip
+  by_zip <- zip_tract |>
+    select(geoid, zip) |>
+    rename("GEOID" = "geoid") |>
+    inner_join(
+      tract_data,
+      by = "GEOID"
+    ) |>
+    mutate(county = substr(GEOID, 1, 5)) |>
+    group_by(zip)
+  
+  # compute zip-level population size by aggregating across overlapping tracts
+  all_colnames <- names(tract_data)
+  pstrat_colnames <- all_colnames[grepl("male|female", all_colnames)]
+  pstrat_data <- by_zip |>
+    summarise(
+      county = first(county),
+      across(all_of(pstrat_colnames), ~ sum(.x, na.rm = TRUE))
+    )
+  
+  # omit zips with only NA then compute zip-level quantities
+  covar_colnames <- setdiff(all_colnames, pstrat_colnames)
+  covariates <- by_zip |>
+    filter(if_all(all_of(covar_colnames), ~ !all(is.na(.)))) |>
+    summarize(
+      county = first(county),
+      urbanicity  = 1 - sum((pop_size / sum(pop_size, na.rm = TRUE)) * (urbanicity == "N"), na.rm = TRUE),
+      college     = sum(above_college, na.rm = TRUE) / (sum(below_college, na.rm = TRUE) + sum(above_college, na.rm = TRUE)),
+      employment  = sum(employed, na.rm = TRUE) / (sum(employed, na.rm = TRUE) + sum(unemployed, na.rm = TRUE) + sum(other, na.rm = TRUE)),
+      poverty     = sum(`0-0.99`, na.rm = TRUE) / (sum(`0-0.99`, na.rm = TRUE) + sum(`1-1.99`, na.rm = TRUE) + sum(`2+`, na.rm = TRUE)),
+      income      = sum((pop_size / sum(pop_size, na.rm = TRUE)) * household_income, na.rm = TRUE),
+      ADI         = sum((pop_size / sum(pop_size, na.rm = TRUE)) * adi, na.rm = TRUE)
+    )
+  
+  return(list(pstrat_data, covariates))
 }
