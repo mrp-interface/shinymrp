b78443a22441746714f0cd3075ce4b6bed5ea1d2
Toan Tran <trannttoan97@gmail.com>
2025-05-29 16:31:10 -0400

    - Update Stan-code generating functions to make gq_data optional - Update Stan-code generation unit tests to use run_mcmc - Update make_standata to increment columns with 1s and 0s programmatically instead of explicitly specifying the columns

--- tests/testthat/test-fct_model.R
+++ tests/testthat/test-fct_model.R
@@ -1,114 +1,302 @@
-create_test_data <- function(N=100, seed=sample(1:10000, 1)) {
+create_test_data <- function(
+    N = 100,
+    seed = sample.int(1e4, 1),
+    params = list(
+      cont = list(lower = 0,  upper = 10, n = 3),
+      bin  = list(n = 3),
+      cat  = list(categories = 1:3, n = 3)
+    ),
+    family = c("binomial", "gaussian")
+) {
+  family <- match.arg(family)
   set.seed(seed)
-  dat <- data.frame(
-    cont1 = runif(N, 0, 10),
-    cont2 = runif(N, 0, 10),
-    bin1 = sample(0:1, N, replace = TRUE),
-    bin2 = sample(0:1, N, replace = TRUE),
-    cat1 = sample(1:3, N, replace = TRUE),
-    cat2 = sample(1:5, N, replace = TRUE),
-    positive = sample(1:5, N, replace = TRUE),
-    total = sample(6:10, N, replace = TRUE)
+  out <- list()
+  
+  # continuous --------------------------------------------------------------
+  if (params$cont$n > 0) {
+    mat <- replicate(
+      params$cont$n,
+      runif(N, params$cont$lower, params$cont$upper)
+    )
+    colnames(mat) <- paste0("cont", seq_len(ncol(mat)))
+    out <- c(out, as.data.frame(mat))
+  }
+  
+  # binary ------------------------------------------------------------------
+  if (params$bin$n > 0) {
+    mat <- replicate(
+      params$bin$n,
+      sample(0:1, N, replace = TRUE)
+    )
+    colnames(mat) <- paste0("bin", seq_len(ncol(mat)))
+    out <- c(out, as.data.frame(mat))
+  }
+  
+  # categorical -------------------------------------------------------------
+  if (params$cat$n > 0) {
+    mat <- replicate(
+      params$cat$n,
+      sample(params$cat$categories, N, replace = TRUE)
+    )
+    colnames(mat) <- paste0("cat", seq_len(ncol(mat)))
+    out <- c(out, as.data.frame(mat))
+  }
+  
+  # combine all into one data.frame
+  df <- do.call(
+    data.frame,
+    c(out, list(stringsAsFactors = FALSE))
   )
-
-  return(dat)
-}
-
-compile <- function(effects, dat) {
-  # convert to numeric factors
-  stan_dat <- stan_factor(dat)
-
-  # placeholder because only model compilation is tested
-  gq_dat <- list(
-    temporal = FALSE,
-    subgroup = c()
-  )
-
-  # group effects
-  effects <- effects %>%
-    group_effects(stan_dat) %>%
-    ungroup_effects()
-    
-  # create stan code and compile model
-  make_stancode_mcmc(effects, gq_dat) %>%
-    cmdstanr::write_stan_file() %>%
-    cmdstanr::cmdstan_model()
+  
+  
+  if (family == "binomial") {
+    df %>% mutate(
+      positive = sample(1:5, N, replace = TRUE),
+      total = sample(6:10, N, replace = TRUE)
+    )
+  } else if (family == "gaussian") {
+    df %>% mutate(
+      value = runif(N, 0, 10)
+    )
+  }
 }
 
 test_that("all models compile", {
-  data <- create_test_data(seed = 123)
   intercept_prior <- "normal(0, 5)"
   effect_prior <- "normal(0, 3)"
   struct_prior <- "structured"
 
-  # fixed effect of a continuous variable
+  data <- create_test_data(seed = 1234) %>%
+    stan_factor(ignore_columns = c("positive", "total"))
+
+  # fixed effect of continuous variables only
   expect_no_error(
-    compile(
-      list(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
         Intercept = list(Intercept = intercept_prior),
-        fixed = list(cont1 = effect_prior)
-      ),
-      data
+        fixed = list(
+          cont1 = effect_prior,
+          cont2 = effect_prior,
+          cont3 = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
     )
   )
 
-  # fixed effects of a binary variables only
+  # fixed effects of binary variables only
   expect_no_error(
-    compile(
-      list(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
         Intercept = list(Intercept = intercept_prior),
-        fixed = list(bin1 = effect_prior)
-      ),
-      data
+        fixed = list(
+          bin1 = effect_prior,
+          bin2 = effect_prior,
+          bin3 = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
     )
   )
 
-  # fixed effects of a categorical variables only
+  # fixed effects of categorical variables only
   expect_no_error(
-    compile(
-      list(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
         Intercept = list(Intercept = intercept_prior),
-        fixed = list(cat1 = effect_prior)
-      ),
-      data
+        fixed = list(
+          cat1 = effect_prior,
+          cat2 = effect_prior,
+          cat3 = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
     )
   )
 
   # varying effects of categorical variables only
   expect_no_error(
-    compile(
-      list(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
         Intercept = list(Intercept = intercept_prior),
-        varying = list(cat1 = effect_prior)
-      ),
-      data
+        varying = list(
+          cat1 = effect_prior,
+          cat2 = effect_prior,
+          cat3 = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
     )
   )
 
   # interaction between fixed effects of
-  # continous variables (without structured prior)
+  # continous variables
   expect_no_error(
-    compile(
-      list(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
         Intercept = list(Intercept = intercept_prior),
-        fixed = list(cont1 = effect_prior, cont2 = effect_prior),
-        interaction = list(`cont1:cont2` = effect_prior)
-      ),
-      data
+        fixed = list(
+          cont1 = effect_prior,
+          cont2 = effect_prior,
+          cont3 = effect_prior
+        ),
+        interaction = list(
+          `cont1:cont2` = effect_prior,
+          `cont2:cont3` = effect_prior,
+          `cont3:cont1` = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
+    )
+  )
+
+  # interaction between fixed effects of
+  # binary variables
+  expect_no_error(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
+        Intercept = list(Intercept = intercept_prior),
+        fixed = list(
+          bin1 = effect_prior,
+          bin2 = effect_prior,
+          bin3 = effect_prior
+        ),
+        interaction = list(
+          `bin1:bin2` = effect_prior,
+          `bin2:bin3` = effect_prior,
+          `bin3:bin1` = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
+    )
+  )
+
+  # interaction between fixed effects of
+  # categorical variables
+  expect_no_error(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
+        Intercept = list(Intercept = intercept_prior),
+        fixed = list(
+          cat1 = effect_prior,
+          cat2 = effect_prior,
+          cat3 = effect_prior
+        ),
+        interaction = list(
+          `cat1:cat2` = effect_prior,
+          `cat2:cat3` = effect_prior,
+          `cat3:cat1` = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
+    )
+  )
+
+  # interaction between varying effects of
+  # categorical variables (without structured prior)
+  expect_no_error(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
+        Intercept = list(Intercept = intercept_prior),
+        varying = list(
+          cat1 = effect_prior,
+          cat2 = effect_prior,
+          cat3 = effect_prior
+        ),
+        interaction = list(
+          `cat1:cat2` = effect_prior,
+          `cat2:cat3` = effect_prior,
+          `cat3:cat1` = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
     )
   )
 
   # interaction between fixed effects of
   # binary variables and continous variables
-  # (without structured prior)
   expect_no_error(
-    compile(
-      list(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
         Intercept = list(Intercept = intercept_prior),
-        fixed = list(bin1 = effect_prior, cont1 = effect_prior),
-        interaction = list(`bin1:cont1` = effect_prior)
-      ),
-      data
+        fixed = list(
+          bin1 = effect_prior,
+          bin2 = effect_prior,
+          cont1 = effect_prior,
+          cont2 = effect_prior
+        ),
+        interaction = list(
+          `bin1:cont1` = effect_prior,
+          `bin1:cont2` = effect_prior,
+          `bin2:cont1` = effect_prior,
+          `bin2:cont2` = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
+    )
+  )
+
+  # interaction between fixed effects of
+  # categorical variables and fixed effects of
+  # continuous variables
+  expect_no_error(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
+        Intercept = list(Intercept = intercept_prior),
+        fixed = list(
+          cat1 = effect_prior,
+          cat2 = effect_prior,
+          cont1 = effect_prior,
+          cont2 = effect_prior
+        ),
+        interaction = list(
+          `cat1:cont1` = effect_prior,
+          `cat1:cont2` = effect_prior,
+          `cat2:cont1` = effect_prior,
+          `cat2:cont2` = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
     )
   )
 
@@ -117,14 +305,57 @@ test_that("all models compile", {
   # categorical variables and fixed effects of
   # continuous variables (without structured prior)
   expect_no_error(
-    compile(
-      list(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
         Intercept = list(Intercept = intercept_prior),
-        fixed = list(cont1 = effect_prior),
-        varying = list(cat1 = effect_prior),
-        interaction = list(`cat1:cont1` = effect_prior)
-      ),
-      data
+        fixed = list(
+          cont1 = effect_prior,
+          cont2 = effect_prior
+        ),
+        varying = list(
+          cat1 = effect_prior,
+          cat2 = effect_prior
+        ),
+        interaction = list(
+          `cat1:cont1` = effect_prior,
+          `cat1:cont2` = effect_prior,
+          `cat2:cont1` = effect_prior,
+          `cat2:cont2` = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
+    )
+  )
+
+  # interaction between fixed effects of
+  # categorical variables and fixed effects of
+  # binary variables
+  expect_no_error(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
+        Intercept = list(Intercept = intercept_prior),
+        fixed = list(
+          bin1 = effect_prior,
+          bin2 = effect_prior,
+          cat1 = effect_prior,
+          cat2 = effect_prior
+        ),
+        interaction = list(
+          `bin1:cat1` = effect_prior,
+          `bin1:cat2` = effect_prior,
+          `bin2:cat1` = effect_prior,
+          `bin2:cat2` = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
     )
   )
 
@@ -132,27 +363,55 @@ test_that("all models compile", {
   # categorical variables and fixed effects of
   # binary variables (without structured prior)
   expect_no_error(
-    compile(
-      list(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
         Intercept = list(Intercept = intercept_prior),
-        fixed = list(bin1 = effect_prior),
-        varying = list(cat1 = effect_prior),
-        interaction = list(`bin1:cat1` = effect_prior)
-      ),
-      data
+        fixed = list(
+          bin1 = effect_prior,
+          bin2 = effect_prior
+        ),
+        varying = list(
+          cat1 = effect_prior,
+          cat2 = effect_prior
+        ),
+        interaction = list(
+          `bin1:cat1` = effect_prior,
+          `bin1:cat2` = effect_prior,
+          `bin2:cat1` = effect_prior,
+          `bin2:cat2` = effect_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
     )
   )
 
+
   # interaction between varying effects of
-  # categorical variables (without structured prior)
+  # categorical variables (with structured prior)
   expect_no_error(
-    compile(
-      list(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
         Intercept = list(Intercept = intercept_prior),
-        varying = list(cat1 = effect_prior, cat2 = effect_prior),
-        interaction = list(`cat1:cat2` = effect_prior)
-      ),
-      data
+        varying = list(
+          cat1 = effect_prior,
+          cat2 = effect_prior,
+          cat3 = effect_prior
+        ),
+        interaction = list(
+          `cat1:cat2` = struct_prior,
+          `cat2:cat3` = struct_prior,
+          `cat3:cat1` = struct_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
     )
   )
 
@@ -160,14 +419,29 @@ test_that("all models compile", {
   # categorical variables and fixed effects of
   # continuous variables (with structured prior)
   expect_no_error(
-    compile(
-      list(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
         Intercept = list(Intercept = intercept_prior),
-        fixed = list(cont1 = effect_prior),
-        varying = list(cat1 = effect_prior),
-        interaction = list(`cat1:cont1` = struct_prior)
-      ),
-      data
+        fixed = list(
+          cont1 = effect_prior,
+          cont2 = effect_prior
+        ),
+        varying = list(
+          cat1 = effect_prior,
+          cat2 = effect_prior
+        ),
+        interaction = list(
+          `cat1:cont1` = struct_prior,
+          `cat1:cont2` = struct_prior,
+          `cat2:cont1` = struct_prior,
+          `cat2:cont2` = struct_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
     )
   )
 
@@ -175,27 +449,29 @@ test_that("all models compile", {
   # categorical variables and fixed effects of
   # binary variables (with structured prior)
   expect_no_error(
-    compile(
-      list(
+    run_mcmc(
+      input_data = data,
+      new_data = data,
+      effects = list(
         Intercept = list(Intercept = intercept_prior),
-        fixed = list(bin1 = effect_prior),
-        varying = list(cat1 = effect_prior),
-        interaction = list(`bin1:cat1` = struct_prior)
-      ),
-      data
-    )
-  )
-
-  # interaction between varying effects of
-  # categorical variables (with structured prior)
-  expect_no_error(
-    compile(
-      list(
-        Intercept = list(Intercept = intercept_prior),
-        varying = list(cat1 = effect_prior, cat2 = effect_prior),
-        interaction = list(`cat1:cat2` = effect_prior)
-      ),
-      data
+        fixed = list(
+          bin1 = effect_prior,
+          bin2 = effect_prior
+        ),
+        varying = list(
+          cat1 = effect_prior,
+          cat2 = effect_prior
+        ),
+        interaction = list(
+          `bin1:cat1` = struct_prior,
+          `bin1:cat2` = struct_prior,
+          `bin2:cat1` = struct_prior,
+          `bin2:cat2` = struct_prior
+        )
+      ) %>%
+        group_effects(data) %>%
+        ungroup_effects(),
+      silent = TRUE
     )
   )
 
